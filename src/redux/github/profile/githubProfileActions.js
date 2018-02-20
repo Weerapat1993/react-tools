16f31b811a6a366aec4ce964c1246f11569effdb
Top <weerapat.thawatchoketawee@gmail.com>
2018-02-20 22:27:30 +0700

    Fix Comment

--- src/redux/github/profile/githubProfileActions.js
+++ src/redux/github/profile/githubProfileActions.js
@@ -3,7 +3,21 @@ import { FETCH_GITHUB_PROFILE, RELOAD_GITHUB_PROFILE } from '../githubActionType
 import { API_ENDPOINT_GITHUB_PROFILE } from '../../../constants/endpoint'
 import { asyncAction } from '../../../utils';
 
-// export const githubProfile2 = (key) => (dispatch, getState) => {
+// export const githubProfileRequest = (key) => ({ type: FETCH_GITHUB_PROFILE.REQUEST, key }) 
+// export const githubProfileSuccess = (key, data) => ({ type: FETCH_GITHUB_PROFILE.SUCCESS, data, key }) 
+// export const githubProfileFailure = (key, error) => ({ type: FETCH_GITHUB_PROFILE.FAILURE, error, key }) 
+// export const githubProfile = (key) => (dispatch, getState) => {
+//   dispatch(githubProfileRequest(key))
+//   return axios({
+//     method: 'GET',
+//     responseType: 'json',
+//     url: API_ENDPOINT_GITHUB_PROFILE(key),
+//   })
+//     .then(res => dispatch(githubProfileSuccess(key, res.data)))
+//     .catch(error => dispatch(githubProfileFailure(key, error)))
+// }
+
+// export const githubProfile = (key) => (dispatch, getState) => {
 //   const action = new AsyncActions(FETCH_GITHUB_PROFILE)
 //   dispatch(action.request(key))
 //   return axios({
@@ -22,20 +36,6 @@ export const githubProfile = (key) => (
     url: API_ENDPOINT_GITHUB_PROFILE(key),
   }, key)
 )
-
-// export const githubProfileRequest = (key) => ({ type: FETCH_GITHUB_PROFILE.REQUEST, key }) 
-// export const githubProfileSuccess = (key, data) => ({ type: FETCH_GITHUB_PROFILE.SUCCESS, data, key }) 
-// export const githubProfileFailure = (key, error) => ({ type: FETCH_GITHUB_PROFILE.FAILURE, error, key }) 
-// export const githubProfile = (key) => (dispatch, getState) => {
-//   dispatch(githubProfileRequest(key))
-//   return axios({
-//     method: 'GET',
-//     responseType: 'json',
-//     url: API_ENDPOINT_GITHUB_PROFILE(key),
-//   })
-//     .then(res => dispatch(githubProfileSuccess(key, res.data)))
-//     .catch(error => dispatch(githubProfileFailure(key, error)))
-// }
 
 export const reloadGithibProfile = (key) => ({ type: RELOAD_GITHUB_PROFILE, key })
 
--- src/redux/github/search/githubSearchActions.js
+++ src/redux/github/search/githubSearchActions.js
@@ -3,26 +3,6 @@ import { SEARCH_GITHUB_REPOSITORIES, CLEAR_GITHUB_REPOSITORIES } from '../github
 import { API_ENDPOINT_SEARCH_GITHUB } from '../../../constants/endpoint'
 import { asyncAction } from '../../../utils'
 
-// export const fetchGithub2 = (keyword) => (dispatch, getState) => {
-//   const action = new AsyncActions(SEARCH_GITHUB_REPOSITORIES)
-//   dispatch(action.request())
-//   return axios({
-//     method: 'GET',
-//     responseType: 'json',
-//     url: API_ENDPOINT_SEARCH_GITHUB(keyword),
-//   })
-//     .then(res => dispatch(action.success(res.data.items)))
-//     .catch(error => dispatch(action.failure(error)))
-// }
-
-export const fetchGithub = (keyword) => (
-  asyncAction(SEARCH_GITHUB_REPOSITORIES, {
-    method: 'GET',
-    responseType: 'json',
-    url: API_ENDPOINT_SEARCH_GITHUB(keyword),
-  })
-)
-
 // export const fetchGithubRequest = () => ({ type: SEARCH_GITHUB_REPOSITORIES.REQUEST }) 
 // export const fetchGithubSuccess = (data) => ({ type: SEARCH_GITHUB_REPOSITORIES.SUCCESS, data }) 
 // export const fetchGithubFailure = (error) => ({ type: SEARCH_GITHUB_REPOSITORIES.FAILURE, error }) 
@@ -33,8 +13,28 @@ export const fetchGithub = (keyword) => (
 //     responseType: 'json',
 //     url: API_ENDPOINT_SEARCH_GITHUB(keyword),
 //   })
-//     .then(res => dispatch(fetchGithubSuccess(res.data.items)))
+//     .then(res => dispatch(fetchGithubSuccess(res.data)))
 //     .catch(error => dispatch(fetchGithubFailure(error)))
 // }
+
+// export const fetchGithub = (keyword) => (dispatch, getState) => {
+//   const action = new AsyncActions(SEARCH_GITHUB_REPOSITORIES)
+//   dispatch(action.request())
+//   return axios({
+//     method: 'GET',
+//     responseType: 'json',
+//     url: API_ENDPOINT_SEARCH_GITHUB(keyword),
+//   })
+//     .then(res => dispatch(action.success(res.data)))
+//     .catch(error => dispatch(action.failure(error)))
+// }
+
+export const fetchGithub = (keyword) => (
+  asyncAction(SEARCH_GITHUB_REPOSITORIES, {
+    method: 'GET',
+    responseType: 'json',
+    url: API_ENDPOINT_SEARCH_GITHUB(keyword),
+  })
+)
 
 export const clearGithubRepositories = () => ({ type: CLEAR_GITHUB_REPOSITORIES })
